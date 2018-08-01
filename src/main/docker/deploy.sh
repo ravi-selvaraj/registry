468ed9e02c4824931f81ecea21ae2d2c88852aca
Ravi Selvaraj <rselvaraj@zuora.com>
2018-08-01 09:46:05 -0700

    Duh Duh !

--- src/main/docker/deploy.sh
+++ src/main/docker/deploy.sh
@@ -2,6 +2,6 @@
 
 cd /idhayangal/registry
 git pull
-docker exec -t maven sh -c "cd /apps/rolodex && mvn clean install"
+docker exec -t maven sh -c "cd /apps/registry && mvn clean install"
 docker exec -t tomcat sh -c "rm -rf /usr/local/tomcat/webapps/registry* && cp /apps/rolodex/target/registry.war /usr/local/tomcat/webapps && ls -l /usr/local/tomcat/webapps"
 docker restart tomcat
